ba149a9413edd985a139416f415526c1859ed199
WICRAMASINGHA W.L.C. <136093147+lakshitha1128@users.noreply.github.com>
2024-04-26 15:27:34 +0530

    change

--- acloath-shop/src/main/java/tshirtPackage/dao/UserDao.java
+++ acloath-shop/src/main/java/tshirtPackage/dao/UserDao.java
@@ -1,5 +1,17 @@
 package tshirtPackage.dao;
 
-public class UserDao {
+import java.sql.*;
+import tshirtPackage.model.*;
 
+public class UserDao {
+	private Connection con;
+
+	private String query;
+    private PreparedStatement pst;
+    private ResultSet rs;
+
+	public UserDao(Connection con) {
+		this.con = con;
+	}
+	
 }
