203e457fde34d3499ae606625bf2022ef037a6e9
WICRAMASINGHA W.L.C. <136093147+lakshitha1128@users.noreply.github.com>
2024-04-27 23:06:19 +0530

    update now LoginServlet

--- acloath-shop/src/main/java/tshirtPackage/servlet/LoginServlet.java
+++ acloath-shop/src/main/java/tshirtPackage/servlet/LoginServlet.java
@@ -10,13 +10,35 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 import tshirtPackage.connection.DbCon;
 
 
 
 @WebServlet("/user-login")
 public class LoginServlet extends HttpServlet {
-	
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		try (PrintWriter out = response.getWriter()) {
+			String email = request.getParameter("login-email");
+			String password = request.getParameter("login-password");
+
+			UserDao udao = new UserDao(DbCon.getConnection());
+			User user = udao.userLogin(email, password);
+			if (user != null) {
+				request.getSession().setAttribute("auth", user);
+//				System.out.print("user logged in");
+				response.sendRedirect("index.jsp");
+			} else {
+				out.println("there is no user");
+			}
+
+		} catch (ClassNotFoundException|SQLException e) {
+			e.printStackTrace();
+		} 
 
 	}
-
+}
