c86dd920391b2c665dd865f5a7ab6a29ec9d9ed1
Nadeesha Rupasinghe <136307805+GNSrupasinghe@users.noreply.github.com>
2024-04-27 12:53:24 +0530

    updated OderNowServlet
    added first code in OderNow servlet

--- acloath-shop/src/main/java/tshirtPackage/servlet/OrderNowServlet.java
+++ acloath-shop/src/main/java/tshirtPackage/servlet/OrderNowServlet.java
@@ -1,40 +1,32 @@
 package tshirtPackage.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.*;
+import java.text.SimpleDateFormat;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class OrderNowServlet
- */
-@WebServlet("/OrderNowServlet")
+import tshirtPackage.connection.DbCon;
+import tshirtPackage.dao.*;
+import tshirtPackage.model.*;
+
+
+@WebServlet("/order-now")
 public class OrderNowServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public OrderNowServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html;charset=UTF-8");
+       
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+    
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
