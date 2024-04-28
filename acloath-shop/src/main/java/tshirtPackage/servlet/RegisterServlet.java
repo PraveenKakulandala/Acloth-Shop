e95ea8c110398987d28425c6863a12477969e0f6
Janethra Dissanayake <129816926+chaki-web@users.noreply.github.com>
2024-04-28 20:02:52 +0530

    Update RegisterServlet.java
    Added the codes into the RegisterServlet. and it is connected to Register JSP

--- acloath-shop/src/main/java/tshirtPackage/servlet/RegisterServlet.java
+++ acloath-shop/src/main/java/tshirtPackage/servlet/RegisterServlet.java
@@ -1,41 +1,51 @@
 package tshirtPackage.servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import tshirtPackage.dao.RegisterDao;
+import tshirtPackage.model.UserReg;
+
 /**
- * Servlet implementation class RegisterServlet
+ * @email Ramesh Fadatare
  */
-@WebServlet("/RegisterServlet")
+
+@WebServlet("/register")
 public class RegisterServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public RegisterServlet() {
-        super();
-        // TODO Auto-generated constructor stub
+    private static final long serialVersionUID = 1L;
+    private RegisterDao employeeDao;
+
+    public void init() {
+        employeeDao = new RegisterDao();
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+     
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+    
+        UserReg employee = new UserReg();
+        employee.setId(0);
+        employee.setName(name);
+        employee.setEmail(email);
+        employee.setPassword(password);
+     
 
+        try {
+            employeeDao.registerEmployee(employee);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        response.sendRedirect("login.jsp");
+    }
 }
