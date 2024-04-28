375a19c4fcec22fc71fb7b33ef0eed00a3029bd2
AshiniNimna <ashininimna1376@gmail.com>
2024-04-28 22:32:48 +0530

    updated remove from cart servelet
    updated remove from cart functionality

--- acloath-shop/src/main/java/tshirtPackage/servlet/RemoveFormCartServlet.java
+++ acloath-shop/src/main/java/tshirtPackage/servlet/RemoveFormCartServlet.java
@@ -18,7 +18,26 @@ public class RemoveFromCartServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
+		response.setContentType("text/html;charset=UTF-8");
+		try (PrintWriter out = response.getWriter()) {
+			String bookId = request.getParameter("id");
+			if (bookId != null) {
+				ArrayList<Cart> cart_list = (ArrayList<Cart>) request.getSession().getAttribute("cart-list");
+				if (cart_list != null) {
+					for (Cart c : cart_list) {
+						if (c.getId() == Integer.parseInt(bookId)) {
+							cart_list.remove(cart_list.indexOf(c));
+							break;
+						}
+					}
+				}
+				response.sendRedirect("cart.jsp");
+
+			} else {
+				response.sendRedirect("cart.jsp");
+			}
+
+		}
 	}
 
 }
