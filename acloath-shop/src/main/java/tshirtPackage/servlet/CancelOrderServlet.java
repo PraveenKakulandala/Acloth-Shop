bc0251b3f48f07057369ccdc2c4ed9a15460c818
kavixz00 <kavixzneth2002@gmail.com>
2024-04-28 22:52:39 +0530

    Updated the servlet for cancel order functionality

--- acloath-shop/src/main/java/tshirtPackage/servlet/CancelOrderServlet.java
+++ acloath-shop/src/main/java/tshirtPackage/servlet/CancelOrderServlet.java
@@ -4,6 +4,11 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import tshirtPackage.connection.DbCon;
 import tshirtPackage.dao.OrderDao;
@@ -14,7 +19,17 @@ public class CancelOrderServlet extends HttpServlet {
     
   
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    
+    try(PrintWriter out = response.getWriter()) {
+      String id = request.getParameter("id");
+      if(id != null) {
+        OrderDao orderDao = new OrderDao(DbCon.getConnection());
+        orderDao.cancelOrder(Integer.parseInt(id));
+      }
+      response.sendRedirect("orders.jsp");
+    } catch (ClassNotFoundException|SQLException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } 
   }
 
 }
