dfbbdf4d122c4e5c9cc076ee82395bd1a33d7b63
AshiniNimna <ashininimna1376@gmail.com>
2024-04-28 22:19:41 +0530

    updated remove from cart servlet

--- acloath-shop/src/main/java/tshirtPackage/servlet/RemoveFormCartServlet.java
+++ acloath-shop/src/main/java/tshirtPackage/servlet/RemoveFormCartServlet.java
@@ -1,41 +1,24 @@
 package tshirtPackage.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class RemoveFormCartServlet
- */
-@WebServlet("/RemoveFormCartServlet")
-public class RemoveFormCartServlet extends HttpServlet {
+import tshirtPackage.model.Cart;
+
+@WebServlet("/remove-from-cart")
+public class RemoveFromCartServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public RemoveFormCartServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
 	}
 
 }
